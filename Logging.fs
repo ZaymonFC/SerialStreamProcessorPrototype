f836fe86d528a8647ac98b76e7c95a7bae3e45b5
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2021-03-24 16:07:12 +1000

    🚀 Add banner log utility

--- Logging.fs
+++ Logging.fs
@@ -6,7 +6,11 @@ let private white = ConsoleColor.White
 let private black = ConsoleColor.Black
 let private red = ConsoleColor.Red
 
-let lockObj = obj()
+let private lockObj = obj()
+
+let private log (s: string) =
+    lock lockObj (fun () -> Console.WriteLine(s))
+
 let private logWithColor fc bg (sev: string) (logLine: string) =
     lock lockObj (fun () ->
         Console.ForegroundColor <- fc
@@ -15,12 +19,18 @@ let private logWithColor fc bg (sev: string) (logLine: string) =
         Console.ResetColor()
         Console.WriteLine($" {logLine}"))
 
+let private logLine componentName message = $"[{componentName}]: {message}"
+
 let logInfo componentName message =
-    let logLine = $"[{componentName}]: {message}"
     let sev = "INF:"
-    logWithColor white blue sev logLine
+    logWithColor white blue sev (logLine componentName message)
 
 let logError componentName message =
-    let logLine = $"[{componentName}]: {message}"
     let sev = "ERR:"
-    logWithColor red black sev logLine
+    logWithColor red black sev (logLine componentName message)
+
+let logBanner componentName message =
+    let sev = "BANNER: "
+    log "---------------"
+    logWithColor white blue sev (logLine componentName message)
+    log "---------------"
